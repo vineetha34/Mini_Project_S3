9e9beddaccbaa60a78bc1a5fc28ebda8f177ad24
vineetha34 <75126810+vineetha34@users.noreply.github.com>
2022-03-04 23:08:28 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+  <div class="az-footer">
+        <div class="container-fluid">
+          <span>&copy; 2022 Insurance Management System (IMS)</span>
+         
+        </div><!-- container -->
+      </div>
